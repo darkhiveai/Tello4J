b2a6144ddfa53e85920b1fb9c33ecbe9ee374d34
Steven Turner <whathasnotbeenused@gmail.com>
2021-11-30 21:12:02 -0800

    Convert throw and go command to static array of bytes.

--- src/main/java/me/friwi/tello4j/wifi/impl/binary/command/TelloBinaryThrowAndGo.java
+++ src/main/java/me/friwi/tello4j/wifi/impl/binary/command/TelloBinaryThrowAndGo.java
@@ -10,6 +10,9 @@ import me.friwi.tello4j.wifi.model.response.TelloResponse;
 import java.io.UnsupportedEncodingException;
 
 public class TelloBinaryThrowAndGo extends TelloBinaryCommand{
+
+    //TODO hack to do throw and go
+    private static final byte[] bytes =  new byte[] {(byte) 0xcc, 0x58, 0x00, 0x7c, 0x48, 0x5d, 0x00, (byte) 0xe4, 0x01, (byte) 0x40, 0x10 };
     @Override
     public byte[] serializeCommand() {
         TelloPacket packet = new TelloPacket();
@@ -18,7 +21,10 @@ public class TelloBinaryThrowAndGo extends TelloBinaryCommand{
         packet.body = new TelloSubPacket();
         packet.body.packetType = PacketTypeValues.TO_TELLO_THROW_AND_GO;
         packet.body.messageID = TelloMessageID.DoThrowTakeoff;
-        return packet.serializeCommand();
+//        packet.body.setSequenceNumber();
+
+
+        return bytes;
     }
 
     @Override
